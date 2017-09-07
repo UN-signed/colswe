36a6669c62c2358d9ccb140226bc439eee63d911
Daniel Caita <dacaitac@unal.edu.co>
2017-09-06 19:27:50 -0500

    Acá se instaló el Bootstrap y se configuró la primera plantilla

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
+class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_index_url
+    get home_index_url
     assert_response :success
   end
 
