8f91b64605568f0e6a2f4c38e698adfaf3ca695f
Daniel Caita <dacaitac@unal.edu.co>
2017-09-26 02:48:58 -0500

    Mail luego de registro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,24 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'colswe.herokuapp.com' }
+
+
+  #Devise and MAILER configuration
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_url_options = { host: 'gmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:     'smarthubcolswe@gmail.com',
+    password:      'colsweapps',
+    domain:        'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
 end
