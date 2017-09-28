d20d36e40c99fdb558def7e53b86dd874811a9ff
Yelis3 <yeli_andre_@hotmail.com>
2017-09-27 23:45:22 -0500

    change my password view added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928022426) do
+ActiveRecord::Schema.define(version: 20170928041040) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.text "key_words"
+    t.text "description"
+    t.string "lenguage"
+    t.text "bibliography"
+    t.string "file_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
