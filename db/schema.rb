bff3ceb130343b3e063bb8578a5196f2c2c2b06e
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2017-09-27 21:45:41 -0500

    update user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907202436) do
+ActiveRecord::Schema.define(version: 20170928022426) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,12 +23,14 @@ ActiveRecord::Schema.define(version: 20170907202436) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.text "degree"
+    t.string "github_username"
+    t.string "photo"
+    t.string "departament"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
