7f89aaeeb68f50b869f8b15e546dbb087395bf94
Jesus <jfchavarrom@unal.edu.co>
2017-10-03 21:02:54 +0000

    fix 2: edits projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,6 +41,7 @@ class ProjectsController < ApplicationController
   # PATCH/PUT /projects/1
   # PATCH/PUT /projects/1.json
   def update
+    @project = Project.find(params[:id])
     respond_to do |format|
       if @project.update(project_params)
         format.html { redirect_to @project, notice: 'Project was successfully updated.' }
