eec34b297d44bb43ff05b4c09d59ba9fcd867688
yeliana <yatorresm@unal.edu.co>
2017-10-03 21:02:11 +0000

    Fix edit profile view error

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all.paginate(page: params[:page], per_page: 9)
+    @projects = Project.all.paginate(page: params[:page], per_page: 12)
   end
 
   # GET /projects/1
