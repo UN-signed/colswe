227c80dd873d0462f51425520734e2b261538372
Juan Pablo Ovalle Sánchez <juanpos97@gmail.com>
2017-10-11 05:23:46 -0500

    Sign up with social networks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,4 +25,123 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+  def facebook
+    puts "\n\n\n-------------------------------------------------------\n\n"
+    puts       "------ The entire provider authentication return ------\n\n"
+    puts request.env['omniauth.auth']
+    puts     "\n-------------------------------------------------------\n\n\n"
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    puts "\n\n\n-------------------------------------------------------\n\n"
+    puts       "------ The entire provider authentication return ------\n\n"
+    puts @user.provider
+    puts @user.uid
+    puts @user.password
+    puts @user.name
+    puts @user.email
+    puts @user.photo
+    puts request.env['omniauth.auth'].info.image
+    session[:myphoto] = request.env['omniauth.auth'].info.image
+    puts @user.persisted?
+    puts @user.save
+    puts @user.persisted? || @user.save
+    puts     "\n-------------------------------------------------------\n\n\n"
+
+
+    if @user.persisted? || @user.save
+      # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      # session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+  def github
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+
+    puts "\n\n\n-------------------------------------------------------\n\n"
+    puts       "------ The entire provider authentication return ------\n\n"
+    puts request.env['omniauth.auth']
+    puts     "\n-------------------------------------------------------\n\n\n"
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    puts "\n\n\n-------------------------------------------------------\n\n"
+    puts       "------ The entire provider authentication return ------\n\n"
+    puts @user.provider
+    puts @user.uid
+    puts @user.password
+    puts @user.name
+    puts @user.email
+    puts @user.photo
+    puts request.env['omniauth.auth'].info.image
+    session[:myphoto] = request.env['omniauth.auth'].info.image
+    puts @user.persisted?
+    puts @user.save
+    puts @user.persisted? || @user.save
+    puts     "\n-------------------------------------------------------\n\n\n"
+
+    if @user.persisted? || @user.save
+      sign_in_and_redirect @user #this will throw if @user is not activated
+      # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+    else
+      # session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+  def google_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+
+    puts "\n\n\n-------------------------------------------------------\n\n"
+    puts       "------ The entire provider authentication return ------\n\n"
+    puts request.env['omniauth.auth']
+    puts     "\n-------------------------------------------------------\n\n\n"
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    puts "\n\n\n-------------------------------------------------------\n\n"
+    puts       "------ The entire provider authentication return ------\n\n"
+    puts @user.provider
+    puts @user.uid
+    puts @user.password
+    puts @user.name
+    puts @user.email
+    puts @user.photo
+    puts request.env['omniauth.auth'].info.image
+    session[:myphoto] = request.env['omniauth.auth'].info.image
+    puts @user.persisted?
+    puts @user.save
+    puts @user.persisted? || @user.save
+    puts     "\n-------------------------------------------------------\n\n\n"
+
+    if @user.persisted? || @user.save
+      sign_in_and_redirect @user #this will throw if @user is not activated
+      # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+    else
+      # session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+
+
+  # def createWithOmniauth
+  #   auth =  request.env['omniauth.auth']
+  #   puts "\n\n\n-------------------------------------------------------\n\n"
+  #   puts       "------ The entire provider authentication return ------\n\n"
+  #   puts auth
+  #   puts     "\n-------------------------------------------------------\n\n\n"
+  #   # session[:omniauth] = auth.except('extra')
+  #   # session[:omniauthExtra] = auth['extra']
+  #   user = User.sign_in_from_omniauth(auth)
+  #   if user.save
+  #     puts "user is save"
+  #     sign_in_and_redirect(:user, user)
+  #   end
+  #   # session[:user_id] = user.id
+  #   # redirect_to profile_path
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,12 +32,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable, :omniauth_providers => [:facebook, :github, :google_oauth2]
 
 	protected
 	def confirmation_required?
 	  false
 	end
+
+  def self.from_omniauth(auth)
+    puts "in self.from_omniauth"
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.photo = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
   has_many :articles
   has_many :members
   # belongs_to :research_group
