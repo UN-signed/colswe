e1e03e29e911159fc82b60ccb72cb8b4884fa5fb
Yeliana Andrea Torres Medina <yatorresm@unal.edu.co>
2017-10-26 22:25:49 -0500

    Dashboard design improved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,27 +2,25 @@ require 'will_paginate/array'
 
 class HomeController < ApplicationController
   def index
-    
-
+    projects = Project.all
+    articles = Article.all
+    research_groups = ResearchGroup.all
+    users = User.all
 
     case params[:item]
       when "1"
-        @items = Project.paginate(page: params[:page], per_page: 6)
+        @items = Project.order('created_at DESC')
       when "2"
-        @items = Article.paginate(page: params[:page], per_page: 6)
+        @items = Article.order('created_at DESC')
       when "3"
-        @items = ResearchGroup.paginate(page: params[:page], per_page: 6)
+        @items = ResearchGroup.order('created_at DESC')
       when "4"
-        @items = User.paginate(page: params[:page], per_page: 6)
+        @items = User.order('created_at DESC')
       else
-        projects = Project.all
-        articles = Article.all
-        research_groups = ResearchGroup.all
-        users = User.all
-        @items = (articles + projects + research_groups + users).paginate(page: params[:page], per_page: 6)
+        @items = (articles + projects + research_groups + users)
     end
 
-
+    @items = @items.paginate(page: params[:page], per_page: 8)
 
     respond_to do |format|
       format.html
