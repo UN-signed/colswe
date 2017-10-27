365ee8abfee3e47f4dff0c5130d27246b4ec4503
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2017-10-26 23:46:23 -0500

    fix home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,20 +4,21 @@ class HomeController < ApplicationController
   def index
     case params[:item]
       when "1"
-        @items = Project.order('created_at DESC').paginate(page: params[:page], per_page: 8)
+        @items = Project.order('created_at DESC')
       when "2"
-        @items = Article.order('created_at DESC').paginate(page: params[:page], per_page: 8)
+        @items = Article.order('created_at DESC')
       when "3"
-        @items = ResearchGroup.order('created_at DESC').paginate(page: params[:page], per_page: 8)
+        @items = ResearchGroup.order('created_at DESC')
       when "4"
-        @items = User.order('created_at DESC').paginate(page: params[:page], per_page: 8)
+        @items = User.order('created_at DESC')
       else
-        projects = Project.all
-        articles = Article.all
-        research_groups = ResearchGroup.all
-        users = User.all
-        @items = (articles + projects + research_groups + users).paginate(page: params[:page], per_page: 8)
+        @items = Project.all
+        @items += Article.all
+        @items += ResearchGroup.all
+        @items += User.all
     end
+
+    @items = @items.paginate(page: params[:page], per_page: 8)
 
     respond_to do |format|
       format.html
