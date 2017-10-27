c7f2422b7882bd31612e959781b472a5c21ca58c
Yeliana Andrea Torres Medina <yatorresm@unal.edu.co>
2017-10-26 23:44:29 -0500

    Pagination: 8 per page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,22 +4,20 @@ class HomeController < ApplicationController
   def index
     case params[:item]
       when "1"
-        @items = Project.order('created_at DESC').paginate(page: params[:page], per_page: 6)
+        @items = Project.order('created_at DESC').paginate(page: params[:page], per_page: 8)
       when "2"
-        @items = Article.order('created_at DESC').paginate(page: params[:page], per_page: 6)
+        @items = Article.order('created_at DESC').paginate(page: params[:page], per_page: 8)
       when "3"
-        @items = ResearchGroup.order('created_at DESC').paginate(page: params[:page], per_page: 6)
+        @items = ResearchGroup.order('created_at DESC').paginate(page: params[:page], per_page: 8)
       when "4"
-        @items = User.order('created_at DESC').paginate(page: params[:page], per_page: 6)
+        @items = User.order('created_at DESC').paginate(page: params[:page], per_page: 8)
       else
         projects = Project.all
         articles = Article.all
         research_groups = ResearchGroup.all
         users = User.all
-        @items = (articles + projects + research_groups + users).paginate(page: params[:page], per_page: 6)
+        @items = (articles + projects + research_groups + users).paginate(page: params[:page], per_page: 8)
     end
-
-    @items = @items.paginate(page: params[:page], per_page: 8) 
 
     respond_to do |format|
       format.html
